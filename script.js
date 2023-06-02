cfc88e0e2333ac2e2c2eb69bfc9c4d15b817574a
samueljustus <113977877+samueljustus@users.noreply.github.com>
2023-06-02 20:00:14 +0100

    functional clock

---
+++ script.js
@@ -0,0 +1,31 @@
+const secondsHand = document.querySelector('.sec-hand')
+const minuteHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hours-hand')
+
+function setDate() {
+   const date = new Date()
+   const seconds = date.getSeconds()
+   const secondsInDegree = ((seconds / 60 )* 360) + 90;
+   secondsHand.style.transform = `rotate(${secondsInDegree}deg)`
+
+   const minute = date.getMinutes()
+   const minuteInDegree = ((minute / 60 )* 360) + 90;
+   minuteHand.style.transform = `rotate(${minuteInDegree}deg)`
+
+   const hours = date.getHours()
+   const hoursInDegree = ((hours / 12 )* 360) + 90;
+   hourHand.style.transform = `rotate(${hoursInDegree}deg)`
+}
+
+
+setInterval(setDate, 1000);
+
+
+
+// function setMinute() {
+//    const date = new Date()
+//    const minute = date.getMinutes();
+
+// }
+
+// setInterval(setMinute, 6000);
